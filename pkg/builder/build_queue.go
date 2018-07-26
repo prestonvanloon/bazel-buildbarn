a677ab878a345d1d403d163f6c23216bd9006a35
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-07-26 22:24:46 +0200

    Abbreviate.

--- pkg/builder/build_queue.go
+++ pkg/builder/build_queue.go
@@ -2,13 +2,10 @@ package builder
 
 import (
 	remoteexecution "google.golang.org/genproto/googleapis/devtools/remoteexecution/v1test"
-	"google.golang.org/genproto/googleapis/longrunning"
 	watcher "google.golang.org/genproto/googleapis/watcher/v1"
-
-	"golang.org/x/net/context"
 )
 
 type BuildQueue interface {
-	Execute(ctx context.Context, request *remoteexecution.ExecuteRequest) (*longrunning.Operation, error)
-	Watch(in *watcher.Request, out watcher.Watcher_WatchServer) error
+	remoteexecution.ExecutionServer
+	watcher.WatcherServer
 }

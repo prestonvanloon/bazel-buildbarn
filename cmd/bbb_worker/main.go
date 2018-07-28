5c38f8f474c79067ae979bead4efe7de0ad9f000
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-07-29 00:15:05 +0200

    Add a completely untested worker process.
    Define a simple streaming protocol for sending ExecuteRequest and
    ExecuteResponse messages across. This can be used by a scheduler process
    to distribute work across multiple systems. Let the worker process use
    this protocol.

---
+++ cmd/bbb_worker/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/EdSchouten/bazel-buildbarn/pkg/ac"
+	"github.com/EdSchouten/bazel-buildbarn/pkg/blobstore"
+	"github.com/EdSchouten/bazel-buildbarn/pkg/builder"
+	"github.com/EdSchouten/bazel-buildbarn/pkg/cas"
+	"github.com/EdSchouten/bazel-buildbarn/pkg/proto/scheduler"
+	"github.com/EdSchouten/bazel-buildbarn/pkg/util"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var (
+		s3Endpoint        = flag.String("s3-endpoint", "", "S3 compatible object storage endpoint for the Content Addressable Storage and the Action Cache")
+		s3AccessKeyId     = flag.String("s3-access-key-id", "", "Access key for the object storage")
+		s3SecretAccessKey = flag.String("s3-secret-access-key", "", "Secret key for the object storage")
+		s3Region          = flag.String("s3-region", "", "Region of the object storage")
+		s3DisableSsl      = flag.Bool("s3-disable-ssl", false, "Whether to use HTTP for the object storage instead of HTTPS")
+
+		schedulerAddress = flag.String("scheduler", "", "Address of the scheduler to which to connect")
+	)
+	flag.Parse()
+
+	// Respect file permissions that we pass to os.OpenFile(), os.Mkdir(), etc.
+	syscall.Umask(0)
+
+	// Web server for metrics and profiling.
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		log.Fatal(http.ListenAndServe(":80", nil))
+	}()
+
+	// Create an S3 client. Set the uploader concurrency to 1 to drastically reduce memory usage.
+	// TODO(edsch): Maybe the concurrency can be left alone for this process?
+	session := session.New(&aws.Config{
+		Credentials:      credentials.NewStaticCredentials(*s3AccessKeyId, *s3SecretAccessKey, ""),
+		Endpoint:         s3Endpoint,
+		Region:           s3Region,
+		DisableSSL:       s3DisableSsl,
+		S3ForcePathStyle: aws.Bool(true),
+	})
+	s3 := s3.New(session)
+	uploader := s3manager.NewUploader(session)
+	uploader.Concurrency = 1
+
+	// Storage of content and actions.
+	contentAddressableStorageBlobAccess := blobstore.NewMetricsBlobAccess(
+		blobstore.NewMerkleBlobAccess(
+			blobstore.NewMetricsBlobAccess(
+				blobstore.NewS3BlobAccess(s3, uploader, aws.String("content-addressable-storage"), util.KeyDigestWithoutInstance),
+				"cas_s3")),
+		"cas_merkle")
+	actionCacheBlobAccess := blobstore.NewMetricsBlobAccess(
+		blobstore.NewS3BlobAccess(s3, uploader, aws.String("action-cache"), util.KeyDigestWithInstance),
+		"ac_s3")
+
+	// On-disk caching of content for efficient linking into build environments.
+	if err := os.Mkdir("/cache", 0); err != nil {
+		log.Fatal("Failed to create cache directory: ", err)
+	}
+
+	buildExecutor := builder.NewCachingBuildExecutor(
+		builder.NewLocalBuildExecutor(
+			cas.NewDirectoryCachingContentAddressableStorage(
+				cas.NewHardlinkingContentAddressableStorage(
+					cas.NewBlobAccessContentAddressableStorage(
+						blobstore.NewMetricsBlobAccess(contentAddressableStorageBlobAccess, "cas_build_executor")),
+					util.KeyDigestWithoutInstance, "/cache", 10000, 1<<30),
+				util.KeyDigestWithoutInstance, 1000)),
+		ac.NewBlobAccessActionCache(
+			blobstore.NewMetricsBlobAccess(actionCacheBlobAccess, "ac_build_executor")))
+
+	// Create connection with scheduler.
+	schedulerConnection, err := grpc.Dial(
+		*schedulerAddress,
+		grpc.WithInsecure(),
+		grpc.WithUnaryInterceptor(grpc_prometheus.UnaryClientInterceptor),
+		grpc.WithStreamInterceptor(grpc_prometheus.StreamClientInterceptor))
+	if err != nil {
+		log.Fatal("Failed to create scheduler RPC client: ", err)
+	}
+	schedulerClient := scheduler.NewSchedulerClient(schedulerConnection)
+
+	// Repeatedly ask the scheduler for work.
+	for {
+		err := subscribeAndExecute(schedulerClient, buildExecutor)
+		log.Print("Failed to subscribe and execute: ", err)
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func subscribeAndExecute(schedulerClient scheduler.SchedulerClient, buildExecutor builder.BuildExecutor) error {
+	stream, err := schedulerClient.Subscribe(context.Background())
+	if err != nil {
+		return err
+	}
+	defer stream.CloseSend()
+
+	for {
+		request, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		if err := stream.Send(buildExecutor.Execute(stream.Context(), request)); err != nil {
+			return err
+		}
+	}
+}
